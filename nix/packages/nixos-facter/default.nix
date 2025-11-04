0e2ab877ea7ae19d8e422a2c89239720c20aaddc
Jörg Thalheim <joerg@thalheim.io>
2025-11-04 17:24:52 +0100

    drop versionSuffix
    this relies on git information that might not always be available:
    
    https://github.com/nix-community/nixos-facter/actions/runs/19075237688/job/54489512050
    
    Also triggers uncessary builds

--- nix/packages/nixos-facter/default.nix
+++ nix/packages/nixos-facter/default.nix
@@ -2,7 +2,6 @@ args@{
   # We need the following pragma to ensure deadnix doesn't remove inputs.
   # This package is being called with newScope/callPackage, which means it is only being passed args it defines.
   # We do not use inputs directly in this file, but need it for passing to the tests.
-  flake,
   # deadnix: skip
   inputs,
   # deadnix: skip
@@ -11,16 +10,8 @@ args@{
   pkgs,
   ...
 }:
-let
-  inherit (pkgs) lib;
-in
 pkgs.callPackage ./package.nix {
   hwinfo = perSystem.hwinfo.default;
-
-  # there's no good way of tying in the version to a git tag or branch
-  # so for simplicity's sake we set the version as the commit revision hash
-  # we remove the `-dirty` suffix to avoid a lot of unnecessary rebuilds in local dev
-  versionSuffix = "-${lib.removeSuffix "-dirty" (flake.shortRev or flake.dirtyShortRev)}";
 }
 // {
   passthru.tests = import ./tests args;
